a5958198ea88869f5bc810524e22cc01a2efef20
Kenny <mengyu.king@gmail.com>
2016-09-18 17:09:32 +0800

    generate QR-Code finished

---
+++ QR_Code/QR_Code/GlobalDefine.h
@@ -0,0 +1,36 @@
+//
+//  GlobalDefine.h
+//  QR_Code
+//
+//  Created by Kenny on 16/9/12.
+//  Copyright © 2016年 MengYu. All rights reserved.
+//
+
+#ifndef GlobalDefine_h
+#define GlobalDefine_h
+
+#define SCREEN_WIDTH    [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT   [UIScreen mainScreen].bounds.size.height
+#define SCREEN_SIZE     [UIScreen mainScreen].bounds.size
+
+#define LRRGBAColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(r)/255.0 blue:(r)/255.0 alpha:a]
+
+//判断是否为iPhone
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+//判断是否为iPad
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+//判断是否为ipod
+#define IS_IPOD ([[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+// 判断是否为 iPhone 5SE
+#define iPhone5SE [[UIScreen mainScreen] bounds].size.width == 320.0f && [[UIScreen mainScreen] bounds].size.height == 568.0f
+// 判断是否为iPhone 6/6s
+#define iPhone6_6s [[UIScreen mainScreen] bounds].size.width == 375.0f && [[UIScreen mainScreen] bounds].size.height == 667.0f
+// 判断是否为iPhone 6Plus/6sPlus
+#define iPhone6Plus_6sPlus [[UIScreen mainScreen] bounds].size.width == 414.0f && [[UIScreen mainScreen] bounds].size.height == 736.0f
+//获取系统版本
+#define IOS_SYSTEM_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
+//判断 iOS 8 或更高的系统版本
+#define IOS_VERSION_8_OR_LATER (([[[UIDevice currentDevice] systemVersion] floatValue] >=8.0)? (YES):(NO))
+
+
+#endif /* GlobalDefine_h */
--- QR_Code/QR_Code/ViewController.m
+++ QR_Code/QR_Code/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "GlobalDefine.h"
 #import "Generate/Generate_QR_Code.h"
+#import "CustomIOSAlertView.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIAlertViewDelegate, CustomIOSAlertViewDelegate>
 
 @property (nonatomic) Generate_QR_Code *generate;
 @property (nonatomic) UIAlertController *alterViewController;
@@ -24,16 +26,116 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    CGSize size = CGSizeMake(240, 240);
-    UIImage *image = [self.generate generateString:@"Hello World!" size:size interpolate:YES];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-    imageView.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height/2);
-    [self.view addSubview:imageView];
+
+    [self initButton];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initButton
+{
+    CGSize size = CGSizeMake(200, 200);
+    
+    for (int index=0; index<2; index++) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+
+        [button setFrame:CGRectMake((SCREEN_WIDTH-size.width)/2, (SCREEN_HEIGHT/2-size.height)/2 + SCREEN_HEIGHT*index/2, size.width, size.height)];
+        [button.layer setMasksToBounds:YES];
+        [button.layer setCornerRadius:5.0];
+        
+        button.tag = 100+index;
+        button.backgroundColor = LRRGBAColor(0, 0, 1, 0.8);
+        
+        if (100 == button.tag) {
+            [button setTitle:@"Generate Code"
+                    forState:UIControlStateNormal];
+        } else {
+            [button setTitle:@"Fetch Code"
+                    forState:UIControlStateNormal];
+        }
+        [button addTarget:self
+                   action:@selector(btnAction:)
+         forControlEvents:UIControlEventTouchUpInside];
+//        NSLog(@"%s:%d, button tag:%ld", __FUNCTION__, __LINE__, (long)button.tag);
+        [self.view addSubview:button];
+    }
+}
+
+- (void)btnAction:(UIButton *)btn
+{
+    if (100 == btn.tag) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Generate Code"
+                                                                       message:@"Please input string"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+
+        [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            [textField setPlaceholder:@"Please input string:"];
+            [textField becomeFirstResponder];
+        }];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Confirm"
+                                                  style:UIAlertActionStyleDefault
+                                                handler:^(UIAlertAction * _Nonnull action) {
+                                                    UITextField *textField = alert.textFields.firstObject;
+                                                    NSLog(@"%@", textField.text);
+                                                    [self showGenerateImage:textField.text];
+                                                }]];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                                  style:UIAlertActionStyleCancel
+                                                handler:^(UIAlertAction * _Nonnull action) {
+                                                    
+                                                }]];
+        [self presentViewController:alert
+                           animated:YES
+                         completion:nil];
+    } else {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Fetch Code"
+                                                                       message:@" "
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                                  style:UIAlertActionStyleCancel
+                                                handler:^(UIAlertAction * _Nonnull action) {
+                                                    
+                                                }]];
+        [self presentViewController:alert
+                           animated:YES
+                         completion:nil];
+    }
+}
+
+- (void)showGenerateImage:(NSString *)str
+{
+    CGSize size = CGSizeMake(240, 240);
+    UIImage *image = [self.generate generateString:str
+                                               size:size
+                                        interpolate:YES];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+
+    CustomIOSAlertView *alertView = [[CustomIOSAlertView alloc] init];
+    [alertView setContainerView:imageView];
+    [alertView setButtonTitles:[NSArray arrayWithObjects:@"OK", nil]];
+    [alertView setDelegate:self];
+    [alertView setOnButtonTouchUpInside:^(CustomIOSAlertView *alertView, int buttonIndex) {
+        [alertView close];
+    }];
+    [alertView setUseMotionEffects:YES];
+    [alertView show];
+}
+
+- (void)customIOS7dialogButtonTouchUpInside: (CustomIOSAlertView *)alertView clickedButtonAtIndex: (NSInteger)buttonIndex
+{
+    NSLog(@"Delegate: Button at position %d is clicked on alertView %d.", (int)buttonIndex, (int)[alertView tag]);
+    [alertView close];
+}
+
+- (UIAlertController *)alterViewController
+{
+    if (!_alterViewController) {
+        _alterViewController = [[UIAlertController alloc] init];
+    }
+    return _alterViewController;
 }
 
 - (Generate_QR_Code *)generate
@@ -43,4 +145,5 @@
     }
     return _generate;
 }
+
 @end
